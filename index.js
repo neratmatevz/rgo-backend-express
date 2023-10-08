ce5c6fc6c1e482393fa711c2aee9e0077b8ffbcc
neratmatevz <matevz.nerat@gmail.com>
2023-10-08 20:41:55 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+
+app.use((express.urlencoded({ extended:false })));
+
+const cors = require('cors');
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.status(200).send({
+
+        test: "test"
+    });
+})
+
+app.post('/', (req, res) => {
+
+    let string = req.body.string;
+
+    console.log(string);
+
+    res.status(200);
+});
+
+
+
+app.listen(process.env.PORT || PORT, 
+    () => {console.log('listening on port '+PORT+`. http://localhost:${PORT}`);}
+);
